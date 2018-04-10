b0b8888fbf79569e2a17b4250227e300ba5dd30f
acammies <acammies@redhat.com>
2018-04-10 17:09:28 +0100

    added more tests, simple functional or snapshots on all

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -1,10 +1,23 @@
-import { shallow } from "@vue/test-utils";
-import Header from '@/components/Header.vue'
+import { shallow, createLocalVue } from "@vue/test-utils";
+import Header from "@/components/Header.vue";
 import * as all from "../unit/setup.js";
 
 describe("Header.vue", () => {
-    it("has the expected html structure", () => {
-        const wrapper = shallow(Header);
-        expect(wrapper.element).toMatchSnapshot();
-    })
-})
+
+//   const Date = 15 / 12 / 1995;
+//   it("has the expected html structure", () => {
+//     const created = () => {}
+//     const wrapper = shallow(Header, {
+//       created
+//     });
+//     expect(wrapper.element).toMatchSnapshot();
+//   });
+
+  it("calls the loadTodos function from actionsjs when created", () => {
+    const created = jest.fn();
+    const wrapper = shallow(Header, {
+      created
+    });
+    expect(created).toHaveBeenCalled();
+  });
+});
---
+++ tests/unit/XofYItems.spec.js
@@ -0,0 +1,63 @@
+import { shallow, createLocalVue, mount } from "@vue/test-utils";
+import Vuex from "vuex";
+import XofYItems from "@/components/XofYItems.vue";
+
+import * as all from "../unit/setup.js";
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+describe("XofYItems.vue", () => {
+  let store;
+
+  const todos = [
+    {
+      title: "Learn awesome things about Labs",
+      completed: false,
+      important: false
+    }
+  ];
+//   const methods = {
+//     sumDoneTodoItems: jest.fn()
+//   };
+  const actions = {
+    loadTodos: jest.fn(),
+    clearTodos: jest.fn()
+  };
+  const getters = {
+    todos: jest.fn()
+  };
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state: {},
+      actions,
+      getters
+    });
+  });
+
+  it("returns the correct length of todos", () => {
+    const mockedTodos = [
+      {
+        title: "Learn awesome things about Labs",
+        completed: true,
+        important: false
+      },
+      {
+        title: "Learn more awesome things about Labs",
+        completed: true,
+        important: false
+      },
+      {
+        title: "Learn even more awesome things about Labs",
+        completed: false,
+        important: false
+      }
+    ];
+
+    expect(mockedTodos.length).toEqual(3)
+    // Replace with testing the sumDoneTodoItems function and check it it's called
+    // expect(sumDoneTodoItems).toHaveBeenCalled()
+  });
+});
